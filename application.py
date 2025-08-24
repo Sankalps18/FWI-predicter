b0572005d9110c9056f27c676643fc6a2cb9e7d3
Sankalps18 <sankalps410@gmail.com>
2025-08-24 08:15:20 +0530

    Beanstalk configuration

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
 app = application
-
+ 
 ridge_model = pickle.load(open('models/ridge.pkl', 'rb'))
 standard_scaler = pickle.load(open('models/scaler.pkl', 'rb'))
 
